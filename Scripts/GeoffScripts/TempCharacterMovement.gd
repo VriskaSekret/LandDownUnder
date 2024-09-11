3d2d279b58e34f0fb8031b1de82e2aef2b7e9acf
deminisse <deminisse@gmail.com>
2024-09-11 22:37:59 +1200

    ignore this

--- Scripts/GeoffScripts/TempCharacterMovement.gd
+++ Scripts/GeoffScripts/TempCharacterMovement.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 var dir
-const SPEED = 600.0
+var hp := 80
+const SPEED = 200.0
+
+
+# Enemy Related (temporary)
+var enemy_close = []
 
 func get_input():
 	var input_direction = Input.get_vector("p1left", "p1right", "p1up", "p1down")
 	dir = input_direction
-	print (dir)
 	velocity = input_direction * SPEED
 
 func _physics_process(delta):
@@ -18,3 +22,10 @@ func _physics_process(delta):
 	elif dir[0] > 0:
 		animated_sprite_2d.flip_h = false
 	move_and_slide()
+
+# Enemy Detection shit 
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
