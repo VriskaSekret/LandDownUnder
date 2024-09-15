35fc6f4d917e35279793a56b16c95df3a112b582
Abhijot Malhi <33820960+tabyrocket@users.noreply.github.com>
2024-09-15 19:31:28 +1200

    Adjust vegemite and added bush ranger animations

--- Scenes/TabyScenes/Attacks/Cone/cone.gd
+++ Scenes/TabyScenes/Attacks/Cone/cone.gd
@@ -4,12 +4,13 @@ var level: int = 1
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var cone_area: Area2D = $Sprite2D/ConeArea
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sprite_2d.scale = Vector2(0.5 + 0.5 * level, 0.5 + 0.5 * level)
 	cone_area.level = level
-	apply_impulse(Vector2(100, -700))
+	apply_impulse(Vector2(100 * [1, -1][randi() % 2], -700))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
