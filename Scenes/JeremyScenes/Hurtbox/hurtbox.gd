42430304325fe0c8d90686df280280f2df569dbb
deminisse <deminisse@gmail.com>
2024-09-22 03:04:00 +1200

    Update hurtbox.gd

--- Scenes/JeremyScenes/Hurtbox/hurtbox.gd
+++ Scenes/JeremyScenes/Hurtbox/hurtbox.gd
@@ -7,7 +7,6 @@ extends Area2D
 
 signal hurt(damage, angle, knockback)
 
-var hit_once_arr = []
 
 func _on_area_entered(area):
 	if area.is_in_group("Attack"):
@@ -18,13 +17,9 @@ func _on_area_entered(area):
 					collision.visible = false
 					disable_timer.start()
 				1: #HitOnce
-					if hit_once_arr.has(area) == false:
-						hit_once_arr.append(area)
-						if area.has_signal("remove_from_arr"):
-							if not area.is_connected("remove_from_arr", Callable(self, "remove_from_list")):
-								area.connect("remove_from_arr", Callable(self, "remove_from_list"))
-					else:
-						return
+					if area.has_signal("remove_from_arr"):
+						if not area.is_connected("remove_from_arr", Callable(self, "remove_from_list")):
+							area.connect("remove_from_arr", Callable(self, "remove_from_list"))
 				2: #DisableHitBox
 					if area.has_method("temp_disable"):
 						area.temp_disable()
@@ -42,10 +37,6 @@ func _on_area_entered(area):
 			emit_signal("hurt", damage, angle, knockback)
 			if area.has_method("enemy_hit"):
 				area.enemy_hit(1)
-
-func remove_from_list(obj):
-	if hit_once_arr.has(obj):
-		hit_once_arr.erase(obj)
 
 func _on_disable_timer_timeout():
 	collision.call_deferred("set", "disabled", false)
