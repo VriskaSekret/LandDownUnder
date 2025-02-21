2793261dd457ff7481be28c71b66d0728f3defe1
tabyrocket <abhijotmalhi@gmail.com>
2025-02-22 01:50:24 +1300

    Added debug key (~) to see FPS and enemy count

--- Scenes/JeremyScenes/DEBUGDEBUGDEBUG.gd
+++ Scenes/JeremyScenes/DEBUGDEBUGDEBUG.gd
@@ -1,4 +1,6 @@
 extends Label
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("debug"):
+		visible = not visible
 	text = "FPS: " + str(Engine.get_frames_per_second()) + "\nCurrent Enemies: " + str(get_tree().get_nodes_in_group("active_enemy").size())
